94d40f57bdde5a9cf5b07200908b651a85605d8c
Ömer Faruk <spacepire0000@hotmail.com>
2023-07-15 22:33:52 +0300

    a
    deneme

---
+++ C/c00/ex07/ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odasdemi <odasdemi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/14 02:38:45 by odasdemi          #+#    #+#             */
+/*   Updated: 2023/07/14 02:38:46 by odasdemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		 ft_putchar(nb + 48);
+}
+
+int	main(void)
+{
+	ft_print_combn(88);
+	return (0);
+}
